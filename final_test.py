3b6a80dabb559fa8f7375df488ae95bee15a630d
JRosario <34451372+jaleesarosario@users.noreply.github.com>
2019-12-09 23:29:16 -0500

    Add files via upload

---
+++ final_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+#Finish if you can... stop crying
+class TestFlora(unittest.TestCase):
+
+    def test_colors(self):
+        """
+        Test colors
+        """
+        self.assertTrue(baseurl == 'http://researcharchive.calacademy.org')
+        self.assertTrue(header == {'User-agent': 'Mozilla/5.'})
+        self.assertTrue(page_soup == BeautifulSoup(page_text,'html.parser'))
+        self.assertTrue(wfresults == page_soup.findAll(True, {'class':["wfresultname"]}))
+
+        self.assertFalse(white_wf == baseurl + '/research/botany/wildflow/color.asp?c=' + item)
+        self.assertFalse(wf == baseurl + '/research/botany/wildflow/color.asp?c=' + color)
+        self.assertFalse(page_soup2 == requests.get(white_wf, headers=header).text)
+
+
+    def test_db(self):
+        """
+        Test DB
+        """
+        self.assertEqual(DBNAME == 'Floral.db')
+
+if __name__ == '__main__':
+    unittest.main()
